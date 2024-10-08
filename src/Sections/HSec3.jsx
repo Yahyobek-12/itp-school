9aa6b0f284d6d8cc70c35d228ff66b423eb8d7c3
Yahyobek <caisophiya@gmail.com>
2024-10-08 06:28:12 +0500

    updated files

--- src/Sections/HSec3.jsx
+++ src/Sections/HSec3.jsx
@@ -1,10 +1,20 @@
+import { HSec3Cards } from '../UI'
+
 const HSec3 = () => {
   return (
-    <div className="w-[100%] h-[70vh] bg-slate-900 py-12 px-12">
-        <h1 className="text-5xl font-bold text-white">O‘quvchilarning fikrlari</h1>
-        <div className="w-[100%] h-[90%] bg-red-500 mt-5 flex items-start">
-
+    <div className="w-[100%] h-[90vh] xl:py-12 py-4 xl:px-12 px-4">
+      <h1 className="text-white xs:text-5xl text-3xl font-bold text-center">Nega aynan biz ?</h1>
+      <div className="w-[100%] h-[100%] lg:flex block items-center justify-around">
+        {HSec3Cards.map((item, idx) => (
+          <div key={idx} className="xl:w-[350px] lg:w-[300px] w-[100%] xs:h-[450px] h-[400px] bg-gray-700 rounded-xl py-4 px-4 lg:mt-0 mt-4">
+          <div className="w-[100%] xs:h-[60%] h-[48%] flex items-center justify-center">
+            <img src={item.img} alt="grow-user" className="xs:w-[250px] w-[200px] xs:h-[250px] h-[200px]" />
+          </div>
+          <h1 className="text-white text-2xl mt-3 text-center">{item.title}</h1>
+          <p className="text-gray-300 mt-3 text-center">{item.p}</p>
         </div>
+        ))}
+      </div>
     </div>
   )
 }
---
+++ src/UI.js
@@ -0,0 +1,9 @@
+import growUser from './Images/grow.png'
+import oldPC from './Images/old computer.png'
+import teacher from './Images/teacher.png'
+
+export const HSec3Cards = [
+    {img: growUser, title: 'Shaxsiy Yondashuv', p: "O'quvchilarimiz har biri individual o'quv dasturi asosida ta'lim oladi. Har bir talabaga alohida e'tibor qaratilib, ularning rivojlanishiga mos yondashuv taqdim etiladi."},
+    {img: oldPC, title: 'Amaliy Darslar', p: "Bizning kurslarimiz amaliyotga asoslangan bo'lib, o'quvchilar real loyihalar ustida ishlaydi. Nazariy bilimlarni darhol qo'llash orqali tez va samarali o'rganish ta'minlanad"},
+    {img: teacher, title: "Tajribali O'qituvchilar", p: "Bizning o'qituvchilarimiz IT sohasida katta tajribaga ega va sizga eng yangi bilimlarni o'rgatishga tayyor. Ularning yordami bilan siz texnologiyalarni chuqur o'rganasiz.."}
+]
