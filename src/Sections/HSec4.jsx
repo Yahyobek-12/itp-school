8ec2cbfe4742960acfefbd0f0740af83765a5cfb
Yahyobek <caisophiya@gmail.com>
2024-10-08 06:38:01 +0500

    updated files

--- src/Sections/HSec4.jsx
+++ src/Sections/HSec4.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const HSec4 = () => {
   return (
-    <div className='w-[100%] h-[100vh] flex items-center justify-center'>
-        <h1 className='text-white text-9xl'>Building...</h1>
+    <div className=''>
+        {/* <h1 className='text-white text-9xl'>Building...</h1> */}
     </div>
   )
 }
