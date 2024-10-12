0034aed1c5af69ed3171a095236c033afe9d68f6
Yahyobek <caisophiya@gmail.com>
2024-10-12 14:02:09 +0500

    Last Finish

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -19,7 +19,7 @@ const Navbar = () => {
           <Link to="/courses" className='py-[15px] px-[25px] text-gray-300 font-bold rounded-md hover:bg-slate-800'>Kurslar</Link>
           <Link to="/shop" className='py-[15px] px-[25px] text-gray-300 font-bold rounded-md hover:bg-slate-800'>Do'kon</Link>
         </ul>
-        <Link to="/" className='py-[10px] px-[20px] ml-4 bg-gradient-to-br from-blue-600 via-blue-400 to-blue-600 text-white rounded-md hover:scale-[98%] border-b-[3px] border-blue-800 hidden md:block'>Bog'lanish</Link>
+        <Link to="https://t.me/ALTAQVOOOO" target='_blank' className='py-[10px] px-[20px] ml-4 bg-gradient-to-br from-blue-600 via-blue-400 to-blue-600 text-white rounded-md hover:scale-[98%] border-b-[3px] border-blue-800 hidden md:block'>Bog'lanish</Link>
         {toggle ? <IoClose className='text-white text-[40px] block md:hidden py-2 px-2 z-10' style={{ cursor: 'pointer' }}onClick={clickToggle} /> : <HiMenuAlt4 className='text-white text-[40px] block md:hidden py-2 px-2 z-10' style={{ cursor: 'pointer' }}onClick={clickToggle} />}
 
         <div className={`${toggle ? 'block' : 'hidden'} md:hidden block w-[100%] h-[45vh] fixed top-0 left-0 bg-newBlack px-4`}>
@@ -28,7 +28,7 @@ const Navbar = () => {
             <Link to="/courses" className='text-white block mt-6' onClick={clickToggle}>Kurslar</Link>
             <Link to="/shop" className='text-white block mt-6' onClick={clickToggle}>Do'kon</Link>  
           </ul>
-          <Link to="/" className='py-[10px] px-[20px] bg-gradient-to-br from-blue-600 via-blue-400 to-blue-600 text-white rounded-md hover:opacity-85'>Bog'lanish</Link>
+          <Link to="https://t.me/ALTAQVOOOO" target='_blank' className='py-[10px] px-[20px] bg-gradient-to-br from-blue-600 via-blue-400 to-blue-600 text-white rounded-md hover:opacity-85'>Bog'lanish</Link>
         </div>
     </nav>
   )
