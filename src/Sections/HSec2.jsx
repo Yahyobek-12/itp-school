ca7eddfd3e85638132cd2c686b3d70e67753b9b5
Yahyobek <caisophiya@gmail.com>
2024-10-12 13:16:21 +0500

    Finished

--- src/Sections/HSec2.jsx
+++ src/Sections/HSec2.jsx
@@ -2,7 +2,7 @@
 const HSec2 = () => {
   return (
     <div className="w-[100%] xl:h-[60vh] xs:h-[55vh] h-[45vh] xs:py-12 py-6 xs:px-12 px-6">
-        <div id='future' className="w-[100%] h-[100%] rounded-2xl text-center items-center flex xs:px-8 px-4">
+        <div id='future' className="w-[100%] h-[100%] rounded-2xl text-center items-center justify-center flex xs:px-8 px-4">
           <h1 id='future-title' className='xl:text-9xl lg:text-8xl md:text-7xl xs:text-6xl text-5xl text-white font-bold rounded-2xl py-2 text-start'>Biz Bilan Birga Kelajakni Yarating</h1>
         </div>
     </div>
